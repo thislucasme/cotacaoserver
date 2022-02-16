6434e34f3a68c3c255d59ff88ae07647aa74f4a2
Lucas Silva <lucassilvaee1245@gmail.com>
2022-02-16 17:57:12 -0300

    alteração numero da empresa

--- src/Flags/flag.service.ts
+++ src/Flags/flag.service.ts
@@ -16,6 +16,9 @@ export class FlagService {
 		const codigo = await this.criptoService.publicDecript(cotacaoPayload.codigo, "Success2021");
 		const fornecedor = await this.criptoService.publicDecript(cotacaoPayload.fornecedor, "Success2021");
 
+
+		console.log("===========CÓDIGO========", cotacaoPayload.codigoEmpresa)
+
 		const result = await dadosEmpresa.raw(
 			`select dece.codigo6, dece.item6, deic.fornvenc6  from dece01 as dece,
 			deic01 as deic where dece.codigo6 = deic.codigo6 and dece.item6 = deic.item6
--- src/cotacao/cotacao.controller.ts
+++ src/cotacao/cotacao.controller.ts
@@ -1,36 +1,12 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { restaurar } from 'src/common/cripto';
-import { CotacaoTDOPayload, ItemCotacaoTDO } from 'src/models/types';
 import { CotacaoService } from './cotacao.service';
 
 @Controller("cotacao")
 export class CotacaoController {
 	constructor(private cotacaoService: CotacaoService) { }
 
-	//const result = await this.cotacaoService.getItensCotacao('0000000001', 'AG000002');
-	@Get('teste/:codCotacao/:codFornecedor/:contratoEmpresa/:codigoEmpresa')
-	async teste(@Param('codCotacao') codCotacao: string, @Param('codFornecedor') codFornecedor: string, @Param('contratoEmpresa') contratoEmpresa: string, @Param('codigoEmpresa') codigoEmpresa: string) {
-		//const result = await this.cotacaoService.getItensCotacao(codCotacao, codFornecedor);
-
-		const body: CotacaoTDOPayload = {
-			codigo: codCotacao,
-			fornecedor: codFornecedor,
-			flag: '',
-			contratoEmpresa: contratoEmpresa,
-			codigoEmpresa: codigoEmpresa
-		}
-
-		const total = await this.cotacaoService.calcularTotal(body);
-		//return [result, total];
-		return [total];
-	}
-
-	@Get('empresa/:codigo')
-	async getEmpresa(@Param('codigo') codigo: string) {
-		//const result = await this.cotacaoService.getEmpresaByCodigo(codigo);
-		//return result;
-	}
 
 	@Get('enviar-email')
 	async enviar() {
@@ -53,82 +29,9 @@ export class CotacaoController {
 			res.status(HttpStatus.OK).send(result)
 		}
 	}
-
-
-	@Post("teste-requisicao")
-	async testeRequisicao() {
-		const soma = 3 % 2;
-		const result = soma;
-		const raiz = soma + result / soma + 3.56 * soma;
-		return { soma: raiz }
-	}
-
-	@Post('update')
-	async updateItemCotacao(@Body() body: ItemCotacaoTDO) {
-		try {
-			const result = await this.cotacaoService.updateItemCotacao(body);
-			return result;
-		} catch (e) {
-			return { error: e }
-		}
-
-	}
-	@Post('update-flag-fornecedor')
-	async updateFlag(@Body() body: CotacaoTDOPayload, @Res() res: Response) {
-		return body;
-		// const result = await this.cotacaoService.atualizarFlagVendedor(body);
-
-		// if (result === null) {
-		// 	res.status(HttpStatus.OK).send({ data: 404 });
-		// }
-		//	res.status(HttpStatus.OK).send({ data: 201 });
-	}
-	@Post('update-flag-fornecedor-teste')
-	async updateFlagTest(@Body() body: CotacaoTDOPayload, @Res() res: Response) {
-		const result = await this.cotacaoService.flagUpdate(body);
-
-		if (result.data === null) {
-			if (result.code === 404) {
-				res.status(HttpStatus.NOT_FOUND).send({ data: 404 });
-			}
-			if (result.code === 400) {
-				res.status(HttpStatus.BAD_REQUEST).send({ data: 400, msg: "Confira os dados da payload" });
-			}
-		}
-		res.status(HttpStatus.OK).send({ data: 201 });
-	}
-
-	@Post('verificar-flags')
-	async verificarFlags(@Body() body: CotacaoTDOPayload, @Res() res: Response) {
-		const result = await this.cotacaoService.consultarFlags(body);
-		res.status(HttpStatus.ACCEPTED).send(result)
-	}
-
-
 	@Get('chave')
 	async converter(@Body() body: any) {
 		return restaurar(body.cifra)
 	}
 
-
-	async contarItens() {
-
-	}
-
-
-	@Post('calcular-total')
-	async calcularTotal(@Body() body: CotacaoTDOPayload) {
-		const result = await this.cotacaoService.calcularTotal(body);
-		return result;
-	}
-
-
-
-	@Get('xml')
-	async xml(@Body() body: any, @Res() res: Response) {
-		console.log(body.fornecedores)
-		console.log(restaurar('56C387C3AA7565C5BE7A15C2AAC39E7E78C39C26'));
-		//console.log(js2xml(teste, { compact: true, spaces: 4 }))
-		res.send(body);
-	}
 }
