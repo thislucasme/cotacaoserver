228047a9bd6f8ba410fb1e192e5530f39417fc3c
Lucas Silva <lucassilvaee1245@gmail.com>
2022-02-16 15:37:54 -0300

    testes

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { ClienteService } from 'src/cliente/cliente.service';
 import { CotacaoService } from 'src/cotacao/cotacao.service';
-import { CotacaoTDOPayload } from 'src/models/types';
 
 @Controller('banco')
 export class ClienteController {
@@ -10,15 +9,15 @@ export class ClienteController {
 	@Get()
 	async teste() {
 
-		const body: CotacaoTDOPayload = {
-			codigo: "1ECFFA7D7D9E7A05AAEE",
-			fornecedor: "A6D7FA7D7D9E798E",
-			flag: '',
-			contratoEmpresa: "1EDFFA7D75A6",
-			codigoEmpresa: ''
-		}
+		// const body: CotacaoTDOPayload = {
+		// 	codigo: "1ECFFA7D7D9E7A05AAEE",
+		// 	fornecedor: "A6D7FA7D7D9E798E",
+		// 	flag: '',
+		// 	contratoEmpresa: "1EDFFA7D75A6",
+		// 	codigoEmpresa: ''
+		// }
 
-		return await this.clienteService.getItensCotacao(body.codigo, body.fornecedor, body.contratoEmpresa, "01");
+		;//return await this.clienteService.getItensCotacao(body.codigo, body.fornecedor, body.contratoEmpresa, "01");
 
 		//return this.clienteService.getEmpresas('1EDFFA7D75A6');
 	}
