137fac13c7a1e645a04bd39a7b90e5bf78d7109b
Lucas Silva <lucassilvaee1245@gmail.com>
2022-11-01 15:49:28 -0300

    corrigindo link

--- src/cotacao/cotacao.service.ts
+++ src/cotacao/cotacao.service.ts
@@ -265,7 +265,7 @@ export class CotacaoService {
 
 				// const prefixUrl = 'http://localhost:3005/painel/cotacao/' + fullUrl;
 				const prefixUrl = 'https://cotacaocliente-i4zf6mjuu-thislucasme.vercel.app/painel/cotacao/' + fullUrl;
-//
+//lucas
 				//await enviar emaild
 				const envio = await this.sendEmailTo(payloadEnvioEmail.fornecedores[i].email, prefixUrl, empresa, payloadEnvioEmail.fornecedores[i].nome);
 				const accepted = envio.accepted[0] === payloadEnvioEmail.fornecedores[i].email;
